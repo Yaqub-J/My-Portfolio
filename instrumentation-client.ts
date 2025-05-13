6af93119980bbaa99329d82c403c4ed701275ef1
Yaqub-J <yaqubishaqjae@gmail.com>
2025-05-13 05:10:02 +0100

    Fix-1

---
+++ instrumentation-client.ts
@@ -0,0 +1,14 @@
+// This file configures the initialization of Sentry on the client.
+// The added config here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://c6cb503f4d2d0bb9edb95d7c1d2c981b@o4509313832714240.ingest.de.sentry.io/4509313838678096",
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
+
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
