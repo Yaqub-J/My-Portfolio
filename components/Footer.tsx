c43bf6a68b06710293fba7a7f620a4fec557e779
Yaqub-J <yaqubishaqjae@gmail.com>
2025-05-21 04:24:49 +0100

    Fix 3

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,5 @@
 import { FaLocationArrow } from "react-icons/fa6";
-
+import Image from "next/image";
 import { socialMedia } from "@/data";
 import MagicButton from "./MagicButton";
 
@@ -8,10 +8,12 @@ const Footer = () => {
     <footer className="w-full pt-20 pb-10" id="contact">
       {/* background grid */}
       <div className="w-full absolute left-0 -bottom-72 min-h-96">
-        <img
+        <Image
           src="/footer-grid.svg"
           alt="grid"
-          className="w-full h-full opacity-50 "
+          className="w-full h-full opacity-50"
+          width={1920}
+          height={1080}
         />
       </div>
 
@@ -34,7 +36,7 @@ const Footer = () => {
       </div>
       <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
         <p className="md:text-base text-sm md:font-normal font-light">
-          Copyright © 2025 Yaqub Ja'e. All rights reserved.
+          Copyright © 2025 Yaqub Jae. All rights reserved.
         </p>
 
         <div className="flex items-center md:gap-3 gap-6">
@@ -43,7 +45,7 @@ const Footer = () => {
               key={info.id}
               className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
             >
-              <img src={info.img} alt="icons" width={20} height={20} />
+              <Image src={info.img} alt="icons" width={20} height={20} />
             </div>
           ))}
         </div>
